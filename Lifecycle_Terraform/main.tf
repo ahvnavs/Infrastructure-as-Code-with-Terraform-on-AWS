29db73ce13967c6857b6abffed3204bbb71587d8
Abhinav Sharma <2210152@tcs.com>
2026-05-07 16:48:47 +0530

    lifecycle rule arhument types

--- Lifecycle_Terraform/main.tf
+++ Lifecycle_Terraform/main.tf
@@ -2,3 +2,22 @@ provider "aws" {
     region = var.default_region
 }
 
+resource "aws_instance" "ec2_instance" {
+    ami = ""
+    instance_type = var.ec2_instance_type[1]
+    region = var.default_region
+    availability_zone = var.availability_zone[0]
+    associate_public_ip_address = true
+    tags = var.tags
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = var.s3_bucket_name[0]
+    object_lock_enabled = true
+    provider = var.provider
+    region = var.default_region
+    tags = var.tags
+}
--- Lifecycle_Terraform/variables.tf
+++ Lifecycle_Terraform/variables.tf
@@ -3,3 +3,39 @@ variable "default_region" {
     type = string
     default = "ap-south-1"
 }
+
+variable "provider" {
+    description = "default provider"
+    type = string
+    default = "aws"
+}
+
+variable "ec2_instance_type" {
+    description = "different instance types"
+    type = list(string)
+    default = ["t2.micro", "t2.small", "t3.micro", "t3.small"]
+}
+
+variable "availability_zone" {
+    description = "list of availability zone in 'ap-south-1'"
+    type = list(string)
+    default = ["ap-south-1a","ap-south-1b","ap-south-1c"]
+}
+
+variable "tags" {
+    description = "default tags"
+    type = object({
+        env = string
+        job = string
+    })
+    default = {
+        env = "localdesk"
+        job = "terraform lifecycle"
+    }
+}
+
+variable "s3_bucket_name" {
+    description = "s3 bucket name list"
+    type = list(string)
+    default = ["certainty-acl", "bi-llion-rush", "serve-honour", "bigchill-random"]
+}
