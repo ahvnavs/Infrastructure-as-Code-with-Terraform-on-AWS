f6ca1653d1c42b12d068313e104d6a4e330a46e8
ahvnavs <ahvnav01@gmail.com>
2026-05-19 21:41:47 +0530

    func

--- Functions_terraform/main.tf
+++ Functions_terraform/main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
     region = var.default_region
 }
+
+output "formatted_name_01" {
+    value = local.format_01
+}
+
+output "formatted_name_02" {
+    value = local.format_02
+}
+
+resource "aws_s3_bucket" "bucket01" {
+    bucket = "niggasinghji"
+    tags = merge(var.default_tag,var.new_tag)
+}
--- Functions_terraform/variable.tf
+++ Functions_terraform/variable.tf
@@ -3,3 +3,34 @@ variable "default_region" {
     type = string
     default = "ap-south-1"
 }
+
+variable "project_01" {
+    default = "TESTINTUBE"
+}
+
+variable "hello_nigga"{
+    default = "hello world"
+}
+
+locals {
+    format_01 = lower(var.project_01)
+}
+
+locals {
+    format_02 = upper(replace(var.hello_nigga," ","-"))
+}
+
+
+variable "default_tag" {
+    default = {
+        "env" = "local"
+        "computer" = "laptop"
+    }
+}
+
+variable "new_tag" {
+    default = {
+        "escape" = "room"
+        "bitch" = "aar"
+    }
+}
